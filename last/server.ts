4def2dfa4d2e91820868c3064b453566a646278c
GFVictorique <97880270+GFVictorique@users.noreply.github.com>
2024-01-08 00:07:00 +0800

    Update server.ts

--- last/server.ts
+++ last/server.ts
@@ -4,7 +4,6 @@ import { resolve } from "https://deno.land/std/path/mod.ts";
 const app = new Application();
 const port = 8000;
 
-// 使用 `resolve` 函數確保路徑的正確性
 const staticPath = resolve(Deno.cwd(), "static");
 
 app.use(async (ctx) => {
